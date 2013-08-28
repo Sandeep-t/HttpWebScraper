e76f4ce2e8b10921e9df0d4ada85248b8e5ef8b7
Sandeep-t <sandeep.tiwari@imaginea.com>
2013-08-28 10:11:47 +0530

    Code review changes done

--- HttpWebScraper/src/main/java/com/pramati/webscraper/utils/FileHelper.java
+++ HttpWebScraper/src/main/java/com/pramati/webscraper/utils/FileHelper.java
@@ -1,17 +1,11 @@
 package com.pramati.webscraper.utils;
 
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-import org.apache.log4j.Logger;
-
-import com.pramati.webscraper.client.impl.Response;
-
-public class FileWriter {
+public class FileHelper {
 	
-	private static final Logger LOGGER = Logger.getLogger(FileWriter.class);
 	private static final  int BUFFERSIZE = 1024;
 	
 	
@@ -21,6 +15,7 @@ public class FileWriter {
 		while ((numBytesRead = response.read(byteBuf)) != -1) {
 			out.write(byteBuf, 0, numBytesRead);
 		}
+		
 	}
 		
 	}
